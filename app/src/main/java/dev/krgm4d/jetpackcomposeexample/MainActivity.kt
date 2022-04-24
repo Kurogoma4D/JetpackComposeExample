f30f2934c67adbddfff42891a2266250ee72e99d
Kurogoma4D <highdro0479@gmail.com>
2022-04-24 15:30:41 +0900

    add: lesson 3

--- app/src/main/java/dev/krgm4d/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/dev/krgm4d/jetpackcomposeexample/MainActivity.kt
@@ -1,9 +1,11 @@
 package dev.krgm4d.jetpackcomposeexample
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -21,7 +23,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            JetpackComposeExampleTheme {
+                MessageCard(msg = Message("Android", "Jetpack Compose"))
+            }
         }
     }
 }
@@ -38,18 +42,31 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(text = msg.author, color = MaterialTheme.colors.secondaryVariant)
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true, name = "Light Mode")
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark Mode")
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose"))
+    JetpackComposeExampleTheme {
+        MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose"))
+    }
 }
